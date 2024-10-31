4d3f06442e8274f202318788a472d59e3ab88fa0
PitarchAlexandre <alexandre.pitarch@divcom.ch>
2024-10-31 15:24:58 +0100

    migration du projet dans le master

---
+++ app/Models/Participer_Evenement.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Participer_Evenement extends Model
+{
+    use HasFactory;
+}
