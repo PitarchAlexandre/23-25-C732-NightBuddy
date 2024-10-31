89340a1e57bcff96bec1ad5ba08fe28125284f64
Kylian Di Gaetano <143066922+KylianMikeDiGaetano@users.noreply.github.com>
2024-10-31 13:38:37 +0100

    html to blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,17 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NightBuddy</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="icon" href="img/iconweb.ico" type="image/x-icon"> <!-- Lien vers l'icône -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="icon" href="{{ asset('img/iconweb.ico') }}" type="image/x-icon"> <!-- Lien vers l'icône -->
 </head>
-    <body>
-        <!-- Zone de texte pour le formulaire de demande de devis-->
+<body>
+    <!-- Zone de texte pour le formulaire de demande de devis -->
     <div class="container">
         <h2>Inscription NightBuddy</h2>
-        <form action="" method="post">
+        <form action="{{ route('nom_de_la_route') }}" method="post">
+            @csrf <!-- Token CSRF pour la sécurité -->
             <div class="form-section">
                 <h3>Informations personnelles</h3>
-                <label for="nom" autofocus                                                                                                                                                                                                                                                                                            >Nom :</label>
+                <label for="nom">Nom :</label>
                 <input type="text" id="nom" name="txt-nom" autofocus>
 
                 <label for="prenom">Prénom :</label>
@@ -25,29 +26,27 @@
 
                 <label for="conducteur">Conducteur</label>
                 <input type="checkbox" id="conducteur" name="chk-conducteur">
-
             </div>
-                    <HR>
+            <hr>
             <h2>Crée un compte</h2>
             <label for="Email">E-mail :</label>
             <input type="text" id="Email" name="txt-email" placeholder="exemple@ex.gl">
 
-            <label for="mdp">Mots de passe</label>
-            <input type="text" id="mdp" name="txt-mdp" placeholder="Mots de passe">
-                <HR>
+            <label for="mdp">Mot de passe</label>
+            <input type="password" id="mdp" name="txt-mdp" placeholder="Mot de passe">
+            <hr>
             <button type="submit" class="form-button" name="btn-envoyer" value="envoi">Envoyer</button>
         </form>
     </div>
 
-        <footer>
-            <hr>
-            <!-- Partie avec le CopyRight -->
-            <div class="footer-credential">
-                &copy; 2024 NightBuddy. Tous droits résérvées.
-            </div>
-        </footer>
-    <!--Lien avec le JS -->
-    <script src="../js/main.js"></script>
-    </body>
+    <footer>
+        <hr>
+        <!-- Partie avec le CopyRight -->
+        <div class="footer-credential">
+            &copy; 2024 NightBuddy. Tous droits réservés.
+        </div>
+    </footer>
+    <!-- Lien avec le JS -->
+    <script src="{{ asset('js/main.js') }}"></script>
+</body>
 </html>
-
